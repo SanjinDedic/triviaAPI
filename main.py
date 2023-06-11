60b9cbeea83f44a0c9a436c599fd932a19e7cb56
Sanjin84 <ozrobotix@gmail.com>
2023-06-11 15:18:18 +1000

    ready to update main.py

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ from fastapi import Depends, FastAPI, HTTPException
 from fastapi_jwt_auth import AuthJWT
 from fastapi_jwt_auth.exceptions import AuthJWTException
 from typing import List, Dict
-from validator import *
+from difflib import SequenceMatcher
+from typing import Optional
 
 app = FastAPI()
 
@@ -50,6 +51,10 @@ app.add_middleware(
 app.middleware('http')(custom_allow_origin)
 '''
 
+def similar(s1, s2, threshold=0.6):
+    similarity_ratio = SequenceMatcher(None, s1, s2).ratio()
+    return similarity_ratio >= threshold
+
 def random_color():
     #2/3 colors will have values higher than 130 and 1/3 will be lower than 60
     import random
@@ -68,14 +73,6 @@ class Table(BaseModel):
 class Generator(BaseModel):
     topic: str
     num: int
-
-class SaveJSON(BaseModel):
-    quiz_data: List[Dict]
-    filename: str
-
-class SignUp(BaseModel):
-    name: str
-    password: str
 
 class QuickSignUp(BaseModel):
     name: str
@@ -100,38 +97,10 @@ class User(BaseModel):
     team_name: str
     password: str
 
-# Login endpoint
-@app.post("/login")
-async def login(user: User, Authorize: AuthJWT = Depends()):
-    # Connect to the SQLite database
-    conn = sqlite3.connect("database.db")
-    c = conn.cursor()
-    # Query the database to find a team with the given name and password
-    c.execute("SELECT * FROM teams WHERE name = ? AND password = ?", (user.team_name, user.password))
-    team = c.fetchone()
-
-    # If no team is found, raise an HTTP 401 error
-    if team is None:
-        raise HTTPException(status_code=401, detail="Invalid team name or password")
-
-    # If a team is found, create a JWT token with the team name as the subject
-    access_token = Authorize.create_access_token(subject=user.team_name)
-    return {"access_token": access_token}
-
+# Define a User model for login request validation
 @app.get("/test")
 async def test(request: Request):
       return {"message":"This is a test"}
-
-@app.get("/")
-async def home():
-    conn = sqlite3.connect('database.db')
-    c = conn.cursor()
-    c.execute("SELECT * FROM questions")
-    questions = c.fetchall()
-    return {"length": len(questions)}
-    
-
-from typing import Optional
 
 @app.get("/get_teams_table")
 async def get_teams_table(table_name: Optional[str] = "grokkers"):
@@ -141,7 +110,6 @@ async def get_teams_table(table_name: Optional[str] = "grokkers"):
     teams = c.fetchall()
     conn.close()
     return {"teams": teams}
-
 
 
 #just need to figure out how to get the solved questions list updated in the database
@@ -175,40 +143,6 @@ async def submit_answer(a: Answer, Authorize: AuthJWT = Depends()):
         return {"message": "Incorrect"}
 
 
-@app.post("/save_json")
-async def save_json(request: SaveJSON):
-    data = request.quiz_data
-    filename = request.filename
-    with open(f"quizzes/{filename}.json", "w") as f:
-        json.dump(data, f)
-    return {"message": "File saved successfully"}
-
-
-@app.post("/signup")
-async def signup(team: SignUp):
-    team_name = team.name
-    team_password = team.password
-    team_color = random_color()
-
-    conn = sqlite3.connect('database.db')
-    c = conn.cursor()
-
-    # Check if the team already exists
-    c.execute("SELECT * FROM teams WHERE name = ?", (team_name,))
-    existing_team = c.fetchone()
-
-    if existing_team is not None:
-        return {"message": "Team already exists"}
-
-    # Create a new team
-    c.execute("INSERT INTO teams (name, password, score, solved_questions, color) VALUES (?, ?, 0, ?, ?)",
-              (team_name, team_password, json.dumps([]), team_color))
-    conn.commit()
-
-    return {"message": "Team created successfully"}
-
-
-
 @app.post("/quick_signup")
 async def quick_signup(team: QuickSignUp, Authorize: AuthJWT = Depends()):
     team_name = team.name
@@ -228,12 +162,3 @@ async def quick_signup(team: QuickSignUp, Authorize: AuthJWT = Depends()):
     conn.commit()
     access_token = Authorize.create_access_token(subject=team_name)
     return {"access_token": access_token}
-
-
-
-@app.post("/generate_quiz")
-async def generate(generator: Generator):
-   topic_name= generator.topic
-   qs_number= generator.num
-   generated_json= create_quiz(topic=topic_name, num=qs_number)
-   return generated_json
